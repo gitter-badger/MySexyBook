4a19798283477df8db407d1afbf8db43a0847851
viki53 <viki53000@gmail.com>
2015-05-06 01:47:21 +0200

    Introducing… gulp file!

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+
+gulp.task('less', function() {
+	return gulp.src('public/css/mysexybook.less')
+		.pipe(less())
+		.pipe(concat('mysexybook.min.css'))
+		.pipe(cssmin())
+		.pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('js', function() {
+	return gulp.src(['js/*.js'])
+		.pipe(concat('mysexybook.min.js'))
+		.pipe(uglify({preserveComments: 'some'}))
+		.pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('public/css/*.less', ['less']);
+	gulp.watch('public/js/*.js', ['js']);
+});
+
+gulp.task('default', ['watch']);
