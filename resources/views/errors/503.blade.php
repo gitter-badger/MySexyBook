b49a750120a565d57045f426b7958b0880415229
viki53 <viki53000@gmail.com>
2016-06-16 23:05:01 +0200

    Mintenance page with proper design

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,34 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
-        </div>
-    </body>
+<html lang="fr" id="opening-page">
+<head>
+    <meta charset="UTF-8" />
+    <title>My Sexy Book</title>
+    
+    <!--[if lt IE 9]><script src="{{ url('assets/js/libs/html5shiv.js') }}"></script><![endif]-->
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link href="{{ url('assets/css/mysexybook-opening.css') }}" rel="stylesheet">
+    
+    <meta name="theme-color" content="#820043">
+    <script src="{{ url('assets/js/libs/picturefill.js') }}" async></script>
+    
+    <meta name="robots" content="index,nofollow">
+    
+    <meta property="og:url" content="{{ url('/') }}">
+    <meta property="og:title" content="My Sexy Book">
+    <meta property="og:image" content="{{ url('/assets/img/opening-bg.jpg') }}">
+    
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@My_Sexy_Book">
+    <meta name="twitter:url" content="{{ url('/') }}">
+    <meta name="twitter:title" content="My Sexy Book">
+    <meta name="twitter:description" content="Ouverture bientôt…">
+    <meta name="twitter:image" content="{{ url('/assets/img/opening-bg.jpg') }}">
+</head>
+<body>
+    <main id="opening-content">
+        <h1>My Sexy Book</h1>
+        
+        <p id="opening-info">Ouverture bientôt — <a href="mailto:contact@mysexybook.photo">Contactez-nous</a></p>
+    </main>
+</body>
 </html>
