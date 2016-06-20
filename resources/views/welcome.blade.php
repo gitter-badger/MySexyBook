1212c72337b05a6fec1bf7246f75162bc9055689
Corentin H <contact@corentin-hatte.eu>
2016-06-20 14:07:26 +0200

    Add texts on opening page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
 <main id="page">
 	<header id="home-landing">
 		<div class="container">
-			<h1></h1>
+			<h1>La photo sexy et sociale</h1>
 
-			<h2></h2>
+			<h2>Trouvez un modèle parfait, un photographe créatif ou tout simplement de l'inspiration</h2>
 		</div>
 	</header>
 
